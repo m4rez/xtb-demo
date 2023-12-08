6f83f418de87cc2ced53edc6ce56b6dcab435bbc
Bulvamelounek <ma43k.zez@gmail.com>
2023-12-08 13:19:12 +0100

    xtb-demo-created

---
+++ src/main/java/com/xtb/demo/XtbDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xtb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtbDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XtbDemoApplication.class, args);
+	}
+
+}
